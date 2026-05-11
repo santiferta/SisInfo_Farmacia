4e3f913e15f5682743190b212463afe75d145777
matias <matiasmv2005@gmail.com>
2026-05-11 15:20:13 -0400

    Primer commit

---
+++ backend/src/test/java/com/farmacia/cristoredentor/GestionInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.cristoredentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
